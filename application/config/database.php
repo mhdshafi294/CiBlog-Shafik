deefa682e9d328e65b62ec746a8b83ed26ceef57
Shafik AlNahhas <mhd.shafi2.n@gmail.com>
2021-08-19 13:21:01 +0300

    Fixed some errors in the header view & others

--- application/config/database.php
+++ application/config/database.php
@@ -1,5 +1,5 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
 /*
 | -------------------------------------------------------------------
@@ -77,7 +77,7 @@ $db['default'] = array(
 	'dsn'	=> '',
 	'hostname' => 'localhost',
 	'username' => 'root',
-	'password' => '123456',
+	'password' => '',
 	'database' => 'ciblog',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
